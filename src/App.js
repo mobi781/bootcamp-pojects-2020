b4231c70d6bfa2ec274ce5e8212653397b4a053e
Mubashar Toseef <mrmobi62@gmail.com>
2021-05-27 08:27:31 +0500

    create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Chart from './components/Chart'
+import CountrySelect from './components/CountrySelect'
+import Cards from './components/Cards'
 
 function App() {
   return (
     <div className="App">
-
+      <Cards />
+      <CountrySelect />
+      <Chart />
     </div>
   );
 }
---
+++ src/components/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cards = () => {
+    return (
+        <div>
+            <h1>Cards Section</h1>
+        </div>
+    )
+}
+
+export default Cards
