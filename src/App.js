8a0945f282130b0118445b9e437fa7efda104890
Mubashar Toseef <mrmobi62@gmail.com>
2021-05-29 20:18:30 +0500

    cards section complete with props receive in child component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,45 @@
 import './App.css';
 import Chart from './components/Chart'
 import CountrySelect from './components/CountrySelect'
-// import Cards from './components/Cards'
+import React, { useState, useEffect } from 'react'
 import SimpleCard from './components/Cards'
+import axios from 'axios';
 
 function App() {
+  const [globalData, setGlobalData] = useState({});
+
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const url = 'https://covid19.mathdro.id/api';
+        const { data: { confirmed, deaths, recovered, lastUpdate } } = await axios.get(url);
+        const modifiedData = {
+          confirmed,
+          deaths,
+          recovered,
+          lastUpdate,
+
+        }
+        // console.log(modifiedData);
+        setGlobalData(modifiedData);
+        console.log('globalData :>> ', globalData);
+
+      } catch (error) {
+        console.log('error :>> ', error);
+
+      }
+
+
+    }
+
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <h1>Covid-!9</h1>
-      <p>Last Updated On: </p>
-      <SimpleCard />
+      <p>Last Updated On:{new Date(globalData.lastUpdate).toDateString()} </p>
+      <SimpleCard data={globalData} />
       <CountrySelect />
       <Chart />
     </div>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 // import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 // import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import axios from 'axios';
+
 import styles from './Cards.module.css';
 import CountUp from 'react-countup';
+
 
 const useStyles = makeStyles({
     root: {
@@ -32,82 +33,62 @@ const useStyles = makeStyles({
 
 });
 
-export default function SimpleCard() {
+export default function SimpleCard(props) {
     const classes = useStyles();
     // const classes1 = styles();
-
-    // here the logic start start
-    let [globalData, setGlobalData] = useState({});
-    let [check, setCheck] = useState(false);
-    // const { data: { confirmed, deaths, recovered } } = globalData;
-    useEffect(() => {
-        async function fetchData() {
-            setCheck(true);
-            // const url = 'https://covid19.mathdro.id/api';
-            let response = await fetch('https://covid19.mathdro.id/api');
-            let u = await response.json()
-            setGlobalData(u);
-            setCheck(false);
-            // console.log('response :>> ', u);
-        }
-
-        fetchData();
-
-        // return () => {
-        //     // cleanup
-        // }
-    }, []);
-    console.log('globalData :>> ', globalData);
-    const load = "Loading...";
-    if (check) {
-        return (
-            <>
+    console.log('Incoming data from parent component :>> ');
 
 
-                <Card className={classes.root}>
-                    <CardContent className={styles.infected}>
-                        <Typography variant="h5" component="h2">
-                            INFECTED
-                        </Typography>
-                        <br />
-                        <Typography className={classes.pos} variant="h4" color="blue">
-                            {load}
+    // const load = "Loading...";
+    // if (props.props.confirmed.value) {
+    //     return (
+    //         <>
 
-                        </Typography>
 
-                    </CardContent>
+    //             <Card className={classes.root}>
+    //                 <CardContent className={styles.infected}>
+    //                     <Typography variant="h5" component="h2">
+    //                         INFECTED
+    //                     </Typography>
+    //                     <br />
+    //                     <Typography className={classes.pos} variant="h4" color="blue">
+    //                         {load}
 
-                </Card>
-                <Card className={classes.root} >
-                    <CardContent className={styles.recovered}>
-                        <Typography variant="h5" component="h2">
-                            RECOVERED
-                        </Typography>
-                        <br />
-                        <Typography className={classes.pos} variant="h4" color="textSuccess">
-                            {load}
-                        </Typography>
+    //                     </Typography>
 
-                    </CardContent>
+    //                 </CardContent>
 
-                </Card>
-                <Card className={classes.root}>
-                    <CardContent className={styles.deaths}>
-                        <Typography variant="h5" component="h2">
-                            FATALITIES
-                        </Typography>
-                        <br />
-                        <Typography className={classes.pos} variant="h4" color="textDanger">
-                            {load}
-                        </Typography>
+    //             </Card>
+    //             <Card className={classes.root} >
+    //                 <CardContent className={styles.recovered}>
+    //                     <Typography variant="h5" component="h2">
+    //                         RECOVERED
+    //                     </Typography>
+    //                     <br />
+    //                     <Typography className={classes.pos} variant="h4" color="textSuccess">
+    //                         {load}
+    //                     </Typography>
 
-                    </CardContent>
+    //                 </CardContent>
 
-                </Card>
-            </>
+    //             </Card>
+    //             <Card className={classes.root}>
+    //                 <CardContent className={styles.deaths}>
+    //                     <Typography variant="h5" component="h2">
+    //                         FATALITIES
+    //                     </Typography>
+    //                     <br />
+    //                     <Typography className={classes.pos} variant="h4" color="textDanger">
+    //                         {load}
+    //                     </Typography>
 
-        );
-    }
+    //                 </CardContent>
+
+    //             </Card>
+    //         </>
+
+    //     );
+    // }
 
     return (
         <>
@@ -120,7 +101,7 @@ export default function SimpleCard() {
                         </Typography>
                     <br />
                     <Typography className={classes.pos} variant="h4" color="blue">
-                        <CountUp start={0} end={globalData && globalData.confirmed && globalData.confirmed.value} duration={2.75} separator="," />
+                        <CountUp start={0} end={props.data.confirmed.value} duration={2.75} separator="," />
 
                     </Typography>
 
@@ -134,7 +115,7 @@ export default function SimpleCard() {
                         </Typography>
                     <br />
                     <Typography className={classes.pos} variant="h4" color="textSuccess">
-                        <CountUp start={0} end={globalData && globalData.recovered && globalData.recovered.value} duration={2.75} separator="," />
+                        <CountUp start={0} end={props.data.recovered.value} duration={2.75} separator="," />
                     </Typography>
 
                 </CardContent>
@@ -147,7 +128,7 @@ export default function SimpleCard() {
                         </Typography>
                     <br />
                     <Typography className={classes.pos} variant="h4" color="textDanger">
-                        <CountUp start={0} end={globalData && globalData.deaths && globalData.deaths.value} duration={2.75} separator="," />
+                        <CountUp start={0} end={props.data.deaths.value} duration={2.75} separator="," />
                     </Typography>
 
                 </CardContent>
